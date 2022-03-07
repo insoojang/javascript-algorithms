2c2e3f7f3c6bad24565cec7e732f272f3e11f46e
insoojang <insoo2846@gmail.com>
2022-03-07 22:51:12 +0900

    Add : 9문제

--- src/basic/8-sevenDwarfs.js
+++ src/basic/8-sevenDwarfs.js
@@ -1,20 +1,25 @@
-/*대문자로 이루어진 영어단어가 입력되면 단어에 포함된 ‘A'를 모두 ’#‘으로 바꾸어 출력하는
-프로그램을 작성하세요.*/
-function solution(s) {
-    let answer = ''
-    for (let x of s) {
-        if (x === 'A') answer += '#'
-        else answer += x
+/*
+왕비를 피해 일곱 난쟁이들과 함께 평화롭게 생활하고 있던 백설공주에게 위기가 찾아왔다.
+일과를 마치고 돌아온 난쟁이가 일곱 명이 아닌 아홉 명이었던 것이다.
+아홉 명의 난쟁이는 모두 자신이 "백설 공주와 일곱 난쟁이"의 주인공이라고 주장했다. 뛰어난
+수학적 직관력을 가지고 있던 백설공주는, 다행스럽게도 일곱 난쟁이의 키의 합이 100이 됨을
+기억해 냈다.
+아홉 난쟁이의 키가 주어졌을 때, 백설공주를 도와 일곱 난쟁이를 찾는 프로그램을 작성하시오.
+*/
+
+function solution(arr) {
+    let answer = arr
+    let sum = answer.reduce((a, b) => a + b, 0)
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (sum - (answer[i] + answer[j]) == 100) {
+                answer.splice(j, 1)
+                answer.splice(i, 1)
+            }
+        }
     }
     return answer
 }
 
-function solution2(s) {
-    let answer = s
-    answer = answer.replace(/A/g, '#')
-    return answer
-}
-
-let str = 'BANANA'
-console.log(solution(str))
-console.log(solution2(str))
+let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13]
+console.log(solution(arr))
